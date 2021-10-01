eda1b42b34f0f9767314c17c6d4ebc35c514b7d5
Archit Gupta <guptaarchit137@gmail.com>
2021-10-01 16:55:29 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,5 @@
 from tkinter import *
-# kinter is the standard GUI library for Python. Python when combined with Tkinter provides a fast and easy way to create GUI applications. Tkinter provides a powerful object-oriented interface to the Tk GUI toolkit.
+# Tkinter is the standard GUI library for Python. Python when combined with Tkinter provides a fast and easy way to create GUI applications. Tkinter provides a powerful object-oriented interface to the Tk GUI toolkit.
 
 def callback(r,c):
   global person
